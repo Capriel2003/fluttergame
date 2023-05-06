ab0c0d96b1cb696e5e8edfd6162137f87c8fc518
Capriel2003 <gabrielbraganca20039@gmail.com>
2023-05-05 22:30:01 -0300

    mapinicio

--- assets/images/map/spr_grass_tileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.1" name="spr_grass_tileset" tilewidth="16" tileheight="16" tilecount="870" columns="30">
- <image source="spr_grass_tileset.png" width="480" height="464"/>
-</tileset>
--- assets/images/map/spr_water_waves_animated_tileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.1" name="spr_water_waves_animated_tileset" tilewidth="16" tileheight="16" tilecount="200" columns="10">
- <image source="spr_water_waves_animated_tileset.png" width="160" height="320"/>
-</tileset>
