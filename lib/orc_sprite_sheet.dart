29c724079ee76c216dcc3fe446c398d5a7b362d7
Capriel2003 <gabrielbraganca20039@gmail.com>
2023-06-07 15:59:00 -0300

    inimigo com life

--- lib/orc_sprite_sheet.dart
+++ lib/orc_sprite_sheet.dart
@@ -41,11 +41,32 @@ class OrcSpriteSheet {
         ),
       );
 
+  //Movimentação para todos os lados do Orc
   static SimpleDirectionAnimation get simpleDirectionAnimation =>
       SimpleDirectionAnimation(
         idleLeft: OrcSpriteSheet.idleLeft,
         idleRight: OrcSpriteSheet.idleRight,
         runLeft: OrcSpriteSheet.runLeft,
         runRight: OrcSpriteSheet.runRight,
+      );
+
+  static Future<SpriteAnimation> get receiveDamageRight => SpriteAnimation.load(
+        'orc_.png',
+        SpriteAnimationData.sequenced(
+          amount: 4,
+          stepTime: 0.15,
+          textureSize: Vector2(24, 24),
+          texturePosition: Vector2(0, 96),
+        ),
+      );
+
+  static Future<SpriteAnimation> get receiveDamageLeft => SpriteAnimation.load(
+        'orc_.png',
+        SpriteAnimationData.sequenced(
+          amount: 4,
+          stepTime: 0.1,
+          textureSize: Vector2(24, 24),
+          texturePosition: Vector2(96, 96),
+        ),
       );
 }
--- lib/player.dart
+++ lib/player.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 
 import 'package:bonfire/bonfire.dart';
+import 'package:flameteste/homePage.dart';
 import 'package:flameteste/player_sprite_sheet.dart';
 
 class GameHero extends SimplePlayer with ObjectCollision {
@@ -20,6 +21,27 @@ class GameHero extends SimplePlayer with ObjectCollision {
           )
         ],
       ),
+    );
+  }
+
+  @override
+  void joystickAction(JoystickActionEvent event) {
+    if (event.event == ActionEvent.DOWN && event.id == 1) {
+      _executeAttack();
+    }
+    super.joystickAction(event);
+  }
+
+  void _executeAttack() {
+    simpleAttackMelee(
+      damage: 20,
+      size: Vector2.all(tileSize),
+      //animationRight: PlayerSpriteSheet.attackRight,
+      //animationLeft: PlayerSpriteSheet.attackLeft,
+      //animationTop: PlayerSpriteSheet.attackTop,
+      //animationBottom: PlayerSpriteSheet.attackBottom,
+      //height: tileSize,
+      //width: tileSize,
     );
   }
 }
