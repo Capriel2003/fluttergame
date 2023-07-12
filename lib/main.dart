c018d3ee185cebb14a0d47d310be0d61013cca3e
Capriel2003 <gabrielbraganca20039@gmail.com>
2023-07-12 18:46:37 -0300

    novo

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,9 @@
-
 import 'package:flameteste/Home/homePage.dart';
 import 'package:flameteste/firebaseOptions.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 import 'package:firebase_core/firebase_core.dart';
-
-
 
 import 'authScreen.dart';
 import 'gamePage.dart';
@@ -19,11 +16,8 @@ void main() async {
   ]);
 
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform
-  );
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   WidgetsFlutterBinding.ensureInitialized();
-
 
   runApp(const MyApp());
 }
