9a655e8fbabe6383185e994cd5819db252798752
Capriel2003 <gabrielbraganca20039@gmail.com>
2023-07-12 23:19:32 -0300

    a

--- lib/npc/old_man.dart
+++ lib/npc/old_man.dart
@@ -1,13 +1,17 @@
+import 'dart:developer';
 import 'package:bonfire/bonfire.dart';
 import 'package:flameteste/homePage.dart';
 import 'package:flameteste/player/player_sprite_sheet.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
 
-class OldMan extends SimplePlayer with ObjectCollision {
+class OldMan extends SimpleNpc with TapGesture, ObjectCollision {
   OldMan(Vector2 position)
       : super(
           position: position,
           size: Vector2.all(62),
           speed: 180,
+          initDirection: Direction.down,
           animation: PlayerSpriteSheet.simpleDirectionAnimation,
         ) {
     setupCollision(
@@ -21,4 +25,53 @@ class OldMan extends SimplePlayer with ObjectCollision {
       ),
     );
   }
+
+  @override
+  void onTap() {
+    bool flag = false;
+    log("colidiu");
+    seePlayer(
+      observed: (player) {
+        flag = true;
+      },
+      radiusVision: 3 * tileSize,
+    );
+    if (flag) {
+      _showDialogTalk();
+    }
+  }
+
+  void _showDialogTalk() {
+    gameRef.camera.moveToTargetAnimated(this, zoom: 2, finish: () {
+      TalkDialog.show(
+        gameRef.context,
+        [
+          _speak(text: 'Fala meu mano, como voce ta?', isHero: false),
+          _speak(text: 'To bem, e voce?', isHero: true),
+          _speak(
+              text:
+                  'To indo, mas fiquei sabendo que aqui ta cheio de inimigo, toma cuidado',
+              isHero: false),
+        ],
+        logicalKeyboardKeysToNext: [
+          LogicalKeyboardKey.space,
+          LogicalKeyboardKey.enter,
+        ],
+      );
+    });
+  }
+
+  Say _speak({required String text, required bool isHero}) => Say(
+        text: [
+          TextSpan(text: text),
+        ],
+        person: SizedBox(
+            height: 200,
+            width: 200,
+            child: isHero
+                ? GreenNinjaSpriteSheet.spriteSheet.getSprite(0, 0).asWidget()
+                : GreenNinjaSpriteSheet.spriteSheet.getSprite(1, 1).asWidget()),
+        personSayDirection:
+            isHero ? PersonSayDirection.LEFT : PersonSayDirection.RIGHT,
+      );
 }
